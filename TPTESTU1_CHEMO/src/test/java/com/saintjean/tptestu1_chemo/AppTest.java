614b556063f0be9ecfa2c8a28fbf7ebaa71e5463
Anthony CHEMO <anthonychemo@icloud.com>
2025-11-09 18:33:19 +0100

    Initialisation du projet

---
+++ TPTESTU1_CHEMO/src/test/java/com/saintjean/tptestu1_chemo/AppTest.java
@@ -0,0 +1,13 @@
+package com.saintjean.tptestu1_chemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
