47695a4e7c7ecc0a5bd77dd07439aa1bc4dc184f
Anthony CHEMO <anthonychemo@icloud.com>
2025-11-10 20:03:56 +0100

    Création de la classe Utilisateur

---
+++ TPTESTU1_CHEMO/src/main/java/com/saintjean/tptestu1_chemo/Utilisateur.java
@@ -0,0 +1,57 @@
+package com.saintjean.tptestu1_chemo;
+
+public class Utilisateur {
+	
+	    private int id;
+	    private String nom;
+	    private int age;
+	    private String email;
+	    private String telephone;
+	    private String ville;
+	    private double soldePersonnel;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getNom() {
+			return nom;
+		}
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getTelephone() {
+			return telephone;
+		}
+		public void setTelephone(String telephone) {
+			this.telephone = telephone;
+		}
+		public String getVille() {
+			return ville;
+		}
+		public void setVille(String ville) {
+			this.ville = ville;
+		}
+		public double getSoldePersonnel() {
+			return soldePersonnel;
+		}
+		public void setSoldePersonnel(double soldePersonnel) {
+			this.soldePersonnel = soldePersonnel;
+		}
+	    
+	    
+
+}
