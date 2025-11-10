57f30d8dc7948910b8c4c13b590e242610ce2eb6
Anthony CHEMO <anthonychemo@icloud.com>
2025-11-10 18:40:10 +0100

    Test de la levée d'exception lors du factoriel d'un nombre négatif

--- TPTESTU1_CHEMO/src/main/java/com/saintjean/tptestu1_chemo/OperationMathematique.java
+++ TPTESTU1_CHEMO/src/main/java/com/saintjean/tptestu1_chemo/OperationMathematique.java
@@ -9,7 +9,6 @@ public class OperationMathematique {
         return nombre >= 0;
     }
 
-
 	public static long factoriel(int nombre) throws IllegalParamISIException {
 		if (nombre < 0)
 	        throw new IllegalParamISIException("Le factoriel d’un nombre négatif est impossible !");
